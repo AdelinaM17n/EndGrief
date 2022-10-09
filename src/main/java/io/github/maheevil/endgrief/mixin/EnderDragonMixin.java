31ac783b0b4d79cc2c95babb7d6b88c87786c7e8
Maheesha <maheeshasithum@gmail.com>
2022-10-09 11:31:13 +0530

    Compat with Carpet MiniTweaks

--- src/main/java/io/github/maheevil/endgrief/mixin/EnderDragonMixin.java
+++ src/main/java/io/github/maheevil/endgrief/mixin/EnderDragonMixin.java
@@ -1,22 +1,32 @@
 package io.github.maheevil.endgrief.mixin;
 
 import io.github.maheevil.endgrief.EndGriefMod;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
-import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EnderDragon.class)
-public class EnderDragonMixin {
+public abstract class EnderDragonMixin extends Mob {
+
+    protected EnderDragonMixin(EntityType<? extends Mob> entityType, Level level){
+        super(entityType, level);
+    }
+
     @Redirect(
             method = "checkWalls",
             at = @At(
                     value = "INVOKE",
-                    target = "net/minecraft/world/level/GameRules.getBoolean (Lnet/minecraft/world/level/GameRules$Key;)Z"
+                    target = "net/minecraft/world/level/block/state/BlockState.is (Lnet/minecraft/tags/TagKey;)Z"
             )
     )
-    public boolean checkGameRules(GameRules instance, GameRules.Key<GameRules.BooleanValue> key){
-        return !instance.getBoolean(EndGriefMod.disableDragonGrief);
+    public boolean checkGameRules(BlockState instance, TagKey<Block> tagKey){
+        return this.level.getGameRules().getBoolean(EndGriefMod.disableDragonGrief) || instance.is(tagKey);
     }
 }
